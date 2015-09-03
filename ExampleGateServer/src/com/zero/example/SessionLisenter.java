15500bb58f489b6059ae49a9fa6bf6cda56533ff
zerosoft <zerosoft@vip.qq.com>
2015-09-03 23:03:18 +0800

    工程文件

---
+++ ExampleGateServer/src/com/zero/example/SessionLisenter.java
@@ -0,0 +1,36 @@
+package com.zero.example;
+
+import akka.actor.ActorRef;
+
+import com.avalon.api.ActorSession;
+import com.avalon.api.ClientSessionLinenter;
+import com.avalon.api.internal.IoMessage;
+
+/**
+ * 客户端操作类
+ * 
+ * @author zero
+ *
+ */
+public class SessionLisenter implements ClientSessionLinenter {
+
+	@Override
+	public void receivedMessage(ActorSession clientSession, Object message)
+	{
+		// 收到网络协议的
+	}
+
+	@Override
+	public void receivedActorMessage(ActorRef sender, IoMessage ioMessage)
+	{
+		// 收到其他Actor的协议
+	}
+
+	@Override
+	public void disconnected(boolean graceful)
+	{
+		// TODO 失去网络连接的
+		
+	}
+
+}
