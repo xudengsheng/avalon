3c6ed3dfcb4106b355e52bf6936ba7189478edff
zerosoft <zerosoft@vip.qq.com>
2015-11-16 17:01:56 +0800

    1.0

---
+++ Avalon/src/com/avalon/core/actor/InnerLocalActorCallBack.java
@@ -0,0 +1,42 @@
+package com.avalon.core.actor;
+
+import com.avalon.api.internal.ActorCallBack;
+import com.avalon.api.internal.IoMessagePackage;
+import com.avalon.core.AkkaServerManager;
+import com.avalon.core.message.TransportMessage.CloseConnectionSessions;
+import com.avalon.core.message.TransportMessage.IOSessionReciveMessage;
+
+import akka.actor.ActorRef;
+
+public class InnerLocalActorCallBack implements ActorCallBack {
+
+	private final ActorRef self;
+	public final int sessionActorUId;
+
+	public InnerLocalActorCallBack(int sessionActorUId, ActorRef self) {
+		super();
+		this.self = self;
+		this.sessionActorUId = sessionActorUId;
+	}
+
+
+	@Override
+	public void closed() {
+		CloseConnectionSessions closed = new CloseConnectionSessions();
+		AkkaServerManager.inbox.send(self, closed);
+	}
+
+	@Override
+	public void tellMessage(IoMessagePackage messagePackage) {
+		IOSessionReciveMessage message = new IOSessionReciveMessage(messagePackage);
+		AkkaServerManager.inbox.send(self, message);
+//		self.tell(message, ActorRef.noSender());
+	}
+
+
+	@Override
+	public int getBindSessionId() {
+		return sessionActorUId;
+	}
+
+}
